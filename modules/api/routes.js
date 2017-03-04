2f328f1be1ca2a9efb8f975edc8962a038520182
Azulamaris <99040235@mydavinci.nl>
2017-03-04 12:44:19 +0100

    Removed unnecessary routes and indentation fixed

--- modules/api/routes.js
+++ modules/api/routes.js
@@ -1,16 +1,11 @@
 'use strict';
 var router = require('express').Router(),
-		jwt = require('express-jwt'),
-		config = require('../../config/config'),
-		unless = require('express-unless'),
-		subdomain = require('express-subdomain');
+  jwt = require('express-jwt'),
+  config = require('../../config/config'),
+  unless = require('express-unless'),
+  subdomain = require('express-subdomain');
 const path = require('path');
-
-
 
 // Routes for API version 1
 router.use('/v1', require('./v1/routes'));
-router.get('/connectivity', function(req, res) {res.json({statusCode: 1})});
-router.get('/storage/kaan', function(req, res) {res.sendFile(__dirname + "/storage/kaan.jpg")});
-router.get('/storage/kaan2', function(req, res) {res.sendFile(__dirname + "/storage/kaan2.jpg")});
 module.exports = router
