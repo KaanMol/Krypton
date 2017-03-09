754cb6de7f954aa94deae4a0c28e5c5f42a8fc43
maarten nusteling <ik@maartennusteling.nl>
2017-03-09 20:46:02 +0100

    Added login

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+declare var particlesJS;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+  title = 'app works!';
+
+  constructor () {
+  }
+
+  ngOnInit () {
+    particlesJS.load('particles-js', 'assets/test.json', function() {
+      console.log('callback - particles-js config loaded');
+    });
+    window.addEventListener('resize', () => {
+      this.canvasResize();
+    }, true);
+    this.canvasResize();
+  }
+
+  protected canvasResize() {
+    let canvas = document.getElementById('particles-js').getElementsByTagName('canvas')[0];
+    if (canvas) {
+      canvas.height = window.innerHeight;
+      canvas.width = window.innerWidth;
+    }
+  }
+}
