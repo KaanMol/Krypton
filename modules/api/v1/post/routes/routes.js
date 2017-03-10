50e98cf099c9cbf220d5f9dd492dca6a95628b11
Kaan <99040235@mydavinci.nl>
2017-03-10 10:30:52 +0100

    Updated router

--- modules/api/v1/post/routes/routes.js
+++ modules/api/v1/post/routes/routes.js
@@ -4,8 +4,9 @@ var controller = require('../controllers/controller')
 
 router.get('/:id', function(req,res){controller.byPostId(req, res)});
 router.get('/user/:id', function(req,res){controller.byUser(req, res)});
-router.post('/', function(req,res){controller.createPostTo(req, res)});
-router.delete('/:id', function(req,res){controller.removePost(req, res)});
-router.put('/:id', function(req,res){controller.editPost(req, res)});
+router.post('/', function(req,res){controller.create(req, res)});
+router.post('/:id', function(req,res){controller.createTo(req, res)});
+router.delete('/:id', function(req,res){controller.remove(req, res)});
+router.put('/:id', function(req,res){controller.edit(req, res)});
 
 module.exports = router;
